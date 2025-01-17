a71122a5bae755b6e8a6faaa1f2f611e6677196b
THANUJA87 <thanuja.m87@gmail.com>
2025-01-17 14:44:39 +0530

    secong commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,29 +29,29 @@ function App() {
     <>
          <h4 className='text-center fw-bolder mt-4'>Calculator</h4>
     
-        <div  className='container text-center bg-light shadow  w-25 '>
-        <input className='border rounded p-2 m-3 ' value={input} onChange={e=>setInput(e.target.value)} type="text" placeholder='0' readOnly />
+        <div  className='container text-center bg-light shadow  w-25  ' style={{height:"450px"}}>
+        <input className='border rounded p-2 m-3 display' value={input} onChange={e=>setInput(e.target.value)} type="text" placeholder='0'  readOnly />
       
           <div className='but ms-5'  >
-            <button className='btn' onClick={clearInput}  >AC </button>
-            <button className='btn 'onClick={deleteInput}>Del</button>
-            <button className='btn'onClick={()=>buttonClick('/')}>/</button>
-            <button className='btn'onClick={()=>buttonClick('9')} >9</button>
-            <button className='btn'onClick={()=>buttonClick('8')}>8</button>
-            <button className='btn'onClick={()=>buttonClick('7')}>7</button>
-            <button className='btn'onClick={()=>buttonClick('6')}>6</button>
-            <button className='btn'onClick={()=>buttonClick('5')}>5</button>
-            <button className='btn'onClick={()=>buttonClick('4')} >4</button>
-            <button className='btn'onClick={()=>buttonClick('3')}>3</button>
-            <button className='btn'onClick={()=>buttonClick('2')}>2</button>
-            <button className='btn'onClick={()=>buttonClick('1')}>1</button>
-            <button className='btn'onClick={()=>buttonClick('0')}>0</button>
-            <button className='btn'onClick={()=>buttonClick('+')}>+</button>
-            <button className='btn'onClick={()=>buttonClick('-')}>-</button>
-            <button className='btn'onClick={()=>buttonClick('*')}>*</button>
+            <button style={{ backgroundColor: 'lightblue', }} className='btn' onClick={clearInput}  >AC </button>
+            <button style={{ backgroundColor: 'lightblue',  }}className='btn 'onClick={deleteInput}>Del</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('/')}>/</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('9')} >9</button>
+            <button style={{ backgroundColor: 'lightblue', }} className='btn'onClick={()=>buttonClick('8')}>8</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('7')}>7</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('6')}>6</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('5')}>5</button>
+            <button style={{ backgroundColor: 'lightblue', }} className='btn'onClick={()=>buttonClick('4')} >4</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('3')}>3</button>
+            <button style={{ backgroundColor: 'lightblue', }} className='btn'onClick={()=>buttonClick('2')}>2</button>
+            <button style={{ backgroundColor: 'lightblue', }} className='btn'onClick={()=>buttonClick('1')}>1</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('0')}>0</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('+')}>+</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn'onClick={()=>buttonClick('-')}>-</button>
+            <button style={{ backgroundColor: 'lightblue',  }} className='btn 'onClick={()=>buttonClick('*')}>*</button>
 
 
-            <button className='equal btn ' onClick={calculateResult}>=</button>
+            <button style={{ backgroundColor: 'lightblue', color: 'white' }} className='equal btn ' onClick={calculateResult}>=</button>
           </div>
          </div>
        
